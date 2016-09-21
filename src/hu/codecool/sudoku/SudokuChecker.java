7a3d0a43e857fafe5fede473f6f67efea8867359
csaszarhunor <csaszarhunor@gmail.com>
2016-09-21 11:21:16 +0200

    refactor according to public class solution

--- src/hu/codecool/sudoku/SudokuChecker.java
+++ src/hu/codecool/sudoku/SudokuChecker.java
@@ -1,43 +1,64 @@
 package hu.codecool.sudoku;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class SudokuChecker {
-	
-	private static boolean distinctValues(int[] arr){
-	    Set<Integer> foundNumbers = new HashSet<Integer>();
-	    for (int num : arr) {
-	        if(foundNumbers.contains(num)){
-	            return false;
-	        }
-	        foundNumbers.add(num);
-	    }              
-	    return true;          
+
+	public static boolean isTableValid(int[][] gameField){
+		return areRowsValid(gameField) && areColumnsValid(gameField) && areBlocksValid(gameField);
 	}
 	
-	private static boolean areColumnsValid(int[][] table){
-		int[][] temp = new int[table.length][table[0].length];
-		for(int i = 0; i < table.length; i++){
-			for (int j = 0; j < temp[i].length; j++) {
-				temp[j][i] = table[i][j];
+	private static boolean areRowsValid(int[][] gameField){
+		for (int[] row : gameField) {
+			if (containsDuplicates(row)) return false;
+		}
+		return true;
+	}
+
+	private static boolean areColumnsValid(int[][] gameField){
+		int[][] rotated = rotateMatrix(gameField);
+		return areRowsValid(rotated);
+	}
+
+	private static boolean areBlocksValid(int[][] gameField){
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				int[] block = getBlock(gameField, j, i);
+				if (containsDuplicates(block)) return false;
 			}
 		}
-		
-		for (int i = 0; i < temp.length; i++) {
-			distinctValues(temp[i]);
-		}
 		return true;
 	}
-	
-	public static boolean isValid(int[][] table){
-		for (int i = 0; i < table.length; i++) {
-			if(!distinctValues(table[i])) return false;
+
+	private static boolean containsDuplicates(int[] arr){
+		Integer[] a = new Integer[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			a[i] = arr[i];
 		}
-		
-		areColumnsValid(table);
-		
-		return true;
+	    Set<Integer> hashSet = new HashSet<Integer>(Arrays.asList(a));
+	    return hashSet.size() != arr.length;
+	}
+	
+	private static int[][] rotateMatrix(int[][] gameField){
+		int[][] temp = new int[gameField.length][gameField[0].length];
+		for(int i = 0; i < gameField.length; i++){
+			for (int j = 0; j < temp[i].length; j++) {
+				temp[j][i] = gameField[i][j];
+			}
+		}
+		return temp;
+	}
+	
+	private static int[] getBlock(int[][] gameField, int blockRowIndex, int blockColIndex){
+		int[] result = new int[9];
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				result[i * 3 +j] = gameField[blockRowIndex * 3 + i][blockColIndex * 3 +j];
+			}
+		}
+		return result;
 	}
 	
 	public static void main(String[] args) {
@@ -53,8 +74,7 @@ public class SudokuChecker {
 	        { 2, 8, 7, 4, 1, 9, 6, 3, 5 }, 
 	        { 3, 4, 5, 2, 8, 6, 1, 7, 9 } };
 	        
-//	        SudokuChecker.check(gameField);
-	        System.out.println(SudokuChecker.isValid(gameField));
+	        System.out.println(SudokuChecker.isTableValid(gameField));
 		
 	}
 
